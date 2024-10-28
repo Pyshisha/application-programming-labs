3c307a67646ab6cdadf89c57320656905511fee9
Pyshisha <trollmisha2@gmail.com>
2024-10-28 20:10:57 +0300

    Add files via upload
    Исправил

---
+++ main.py
@@ -0,0 +1,36 @@
+import argparse
+
+from file_read import read_file
+from people_with_927 import find_people_by_phone_code
+
+
+def main() -> None:
+    """
+        Главная функция программы. Обрабатывает аргументы командной строки и выводит результаты.
+    """
+    def get_filename() -> str:
+        parser = argparse.ArgumentParser(description="Найти людей с кодом города 927.")
+        parser.add_argument("filename", type=str, help="Имя файла с анкетами")
+        args = parser.parse_args()
+        return args.filename
+
+    try:
+        data = read_file(get_filename())
+        people_with_code_927 = find_people_by_phone_code(data, "927")
+
+        if people_with_code_927:
+            print("Найденные анкеты с кодом города 927:")
+            for person in people_with_code_927:
+                print('\n'.join(person))
+        else:
+            print("Люди с кодом города 927 не найдены.")
+
+    except FileNotFoundError:
+        print("Ошибка: файл не найден.")
+    except PermissionError:
+        print("Не хватает прав доступа")
+
+
+if __name__ == "__main__":
+    main()
+
